65aeeadc68139b8378262cda72016e5ae06709a8
DominoKiddoo <lucas.ellul@gmail.com>
2025-11-30 12:42:52 +1100

    fixing the STUPID merge i did
    im an idiot im an idiot im an idiot im an idiot im an idiot im an idiot im an idiot im an idiot im an idiot im an idiot im an idiot im an idiot im an idiot im an idiot

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include <Geode/modify/LevelSelectLayer.hpp>
 #include <Geode/modify/BoomScrollLayer.hpp>
 #include <hjfod.gmd-api/include/GMD.hpp>
-#include <Geode/binding/GJGameLevel.hpp> 
+#include <Geode/binding/GJGameLevel.hpp>
 #include <Geode/binding/LocalLevelManager.hpp>
 #include <vector>
 #include <string>
 #include <iostream>
 #include <Geode/modify/FLAlertLayer.hpp>
-
 
 using namespace geode::prelude;
 
@@ -39,28 +38,28 @@ std::vector<std::string> mainLevels = {
 };
 
 static std::map<std::string, std::string> levelMapping = {
-    {"Stereo Madness", "Stereo Madness"},
-    {"Back on Track", "Back on Track"},
-    {"Polargeist", "Polargeist"},
-    {"Dry Out", "Dry Out"},
-    {"Base After Base", "Base After Base"},
-    {"Cant Let Go", "Cant Let Go"},
-    {"Jumper", "Jumper"},
-    {"Time Machine", "Time Machine"},
-    {"Cycles", "Cycles"},
-    {"xStep", "xStep"},
-    {"Clutterfunk", "Clutterfunk"},
-    {"Theory of Everything", "Theory of Everything"},
-    {"Electroman Adventure", "Electroman Adventure"},
-    {"Clubstep", "Clubstep"},
-    {"Electrodynamix", "Electrodynamix"},
-    {"Hexagon Force", "Hexagon Force"},
-    {"Blast Processing", "Blast Processing"},
-    {"Theory of Everything 2", "ToE 2"},          
-    {"Geometrical Dominator", "Geometrical Dominato"}, 
-    {"Deadlocked", "Deadlocked"},
-    {"Fingerdash", "Fingerdash"},
-    {"Dash", "Dash"}
+	{"Stereo Madness", "Stereo Madness"},
+	{"Back on Track", "Back on Track"},
+	{"Polargeist", "Polargeist"},
+	{"Dry Out", "Dry Out"},
+	{"Base After Base", "Base After Base"},
+	{"Cant Let Go", "Cant Let Go"},
+	{"Jumper", "Jumper"},
+	{"Time Machine", "Time Machine"},
+	{"Cycles", "Cycles"},
+	{"xStep", "xStep"},
+	{"Clutterfunk", "Clutterfunk"},
+	{"Theory of Everything", "Theory of Everything"},
+	{"Electroman Adventure", "Electroman Adventure"},
+	{"Clubstep", "Clubstep"},
+	{"Electrodynamix", "Electrodynamix"},
+	{"Hexagon Force", "Hexagon Force"},
+	{"Blast Processing", "Blast Processing"},
+	{"Theory of Everything 2", "ToE 2"},
+	{"Geometrical Dominator", "Geometrical Dominato"},
+	{"Deadlocked", "Deadlocked"},
+	{"Fingerdash", "Fingerdash"},
+	{"Dash", "Dash"}
 };
 
 std::string fixLevelName(const std::string& inputValue) {
@@ -70,19 +69,7 @@ std::string fixLevelName(const std::string& inputValue) {
 		}
 	}
 	return inputValue;
-        level->m_levelType = GJLevelType::Editor;
-        
-        LocalLevelManager::sharedState()->m_localLevels->insertObject(level, 0);
-		std::string cLevel = mainLevels[cpage];
-		std::string message = fmt::format("Copied {}!", cLevel);
-
-        FLAlertLayer::create("Success", message, "OK")->show();
-    } 
-    else {
-        FLAlertLayer::create("Error", "An error occured. Please report this to @DominoKiddoo on discord", "OK")->show();
-    }
 }
-
 
 class $modify(CMLbsc, BoomScrollLayer) {
 	bool init(cocos2d::CCArray* pages, int offset, bool looped, cocos2d::CCArray* dynamicObjects, DynamicScrollDelegate* delegate) {
@@ -99,65 +86,64 @@ class $modify(CMLbsc, BoomScrollLayer) {
 };
 
 void importLevel(std::string const& shortName) {
-    std::string fileName = fmt::format("{}.gmd", shortName);
-    auto path = Mod::get()->getResourcesDir() / fileName;
+	std::string fileName = fmt::format("{}.gmd", shortName);
+	auto path = Mod::get()->getResourcesDir() / fileName;
 
-    auto result = gmd::importGmdAsLevel(path.string());
-    
-    if (result.isOk()) {
-        auto level = result.unwrap();
-        level->m_levelType = GJLevelType::Editor;
-        
-        LocalLevelManager::sharedState()->m_localLevels->insertObject(level, 0);
-        std::string fixedName = fixLevelName(shortName);
-        FLAlertLayer::create(nullptr, "Success", fmt::format("Copied {}!", fixedName), "OK", nullptr, 300.f)->show();
-    } 
-    else {
-        FLAlertLayer::create(nullptr, "Error", "Import failed. Report to DominoKiddoo.", "OK", nullptr, 300.f)->show();
-    }
+	auto result = gmd::importGmdAsLevel(path.string());
+
+	if (result.isOk()) {
+		auto level = result.unwrap();
+		level->m_levelType = GJLevelType::Editor;
+
+		LocalLevelManager::sharedState()->m_localLevels->insertObject(level, 0);
+
+		std::string fixedName = fixLevelName(shortName);
+		FLAlertLayer::create(nullptr, "Success", fmt::format("Copied {}!", fixedName), "OK", nullptr, 300.f)->show();
+	}
+	else {
+		FLAlertLayer::create(nullptr, "Error", "Import failed. Report to DominoKiddoo.", "OK", nullptr, 300.f)->show();
+	}
 }
 
 class $modify(InfoPopupHook, FLAlertLayer) {
-    struct Fields {
-        std::string m_targetFileName;
-    };
+	struct Fields {
+		std::string m_targetFileName;
+	};
 
-    bool init(FLAlertLayerProtocol* p0, char const* title, gd::string desc, char const* btn1, char const* btn2, float width, bool scroll, float height, float textScale) {
-        if (!FLAlertLayer::init(p0, title, desc, btn1, btn2, width, scroll, height, textScale)) {
-            return false;
-        }
+	bool init(FLAlertLayerProtocol* p0, char const* title, gd::string desc, char const* btn1, char const* btn2, float width, bool scroll, float height, float textScale) {
+		if (!FLAlertLayer::init(p0, title, desc, btn1, btn2, width, scroll, height, textScale))
+			return false;
 
 		if (!title || !levelMapping.count(title))
 			return true;
-        if (title) {
-			auto mainLayer = this->getChildByID("main-layer");
-            auto bg = mainLayer ? mainLayer->getChildByID("background") : nullptr;
 
-			auto cButton = CCMenuItemSpriteExtra::create(
-				CCSprite::createWithSpriteFrameName("GJ_duplicateBtn_001.png"),
-				this,
-				menu_selector(InfoPopupHook::onCopyMainLevel)
-			);
-            cButton->setPosition(
-                (cButton->getPositionX() - 61.f),
-                cButton->getPositionY()
-            );			
-            m_buttonMenu->addChild(cButton);
-			m_buttonMenu->updateLayout();
+		auto mainLayer = this->getChildByID("main-layer");
+		auto bg = mainLayer ? mainLayer->getChildByID("background") : nullptr;
 
-            
-        }
+		auto cButton = CCMenuItemSpriteExtra::create(
+			CCSprite::createWithSpriteFrameName("GJ_duplicateBtn_001.png"),
+			this,
+			menu_selector(InfoPopupHook::onCopyMainLevel)
+		);
 
-        return true;
-    }
+		cButton->setPosition(
+			(cButton->getPositionX() - 61.f),
+			cButton->getPositionY()
+		);
 
-    void onCopyMainLevel(CCObject*) {
-		if (cpage != 22 && cpage != 23){
+		m_buttonMenu->addChild(cButton);
+		m_buttonMenu->updateLayout();
+
+		return true;
+	}
+
+	void onCopyMainLevel(CCObject*) {
+		if (cpage != 22 && cpage != 23) {
 			std::string cLevel = mainLevels[cpage];
 			importLevel(cLevel);
-
-		} else {
+		}
+		else {
 			FLAlertLayer::create("Error", fmt::format("You are not on a page with a level!"), "OK")->show();
 		}
-    }
+	}
 };

35ce1e81cfa8f149875310c4512fa1e13335a881
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-12-12 05:51:34 -0500

    commander abraham tower

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,53 @@
-#include <Geode/modify/LevelAreaInnerLayer.hpp>
 #include <Geode/modify/LevelSelectLayer.hpp>
 #include <Geode/modify/FLAlertLayer.hpp>
 #include <Geode/modify/LevelPage.hpp>
 #include <Geode/binding/GJGameLevel.hpp>
 #include <Geode/binding/GameLevelManager.hpp>
 #include <Geode/binding/LocalLevelManager.hpp>
+#include <Geode/binding/LevelAreaInnerLayer.hpp>
 
 using namespace geode::prelude;
 
 bool shouldCopy = false;
 GJGameLevel* levelToCopy = nullptr;
 
+constexpr std::array<std::pair<std::string_view,int>, 3> table{{
+	{"The Tower", 5001},
+	{"The Sewers", 5002},
+	{"The Cellar", 5003},
+	{"The Secret Hollow", 5004}
+}};
+
+constexpr const int* levelNameToLevelID(std::string_view key) {
+	for (auto const& kv : table) {
+		if (kv.first == key) return &kv.second;
+	}
+	return nullptr;
+}
+
 class $modify(InfoPopupHook, FLAlertLayer) {
+
+	struct Fields {
+		~Fields() {
+			shouldCopy = false;
+			levelToCopy = nullptr;
+		}
+	};
 
 	bool init(FLAlertLayerProtocol* p0, char const* title, gd::string desc, char const* btn1, char const* btn2, float width, bool scroll, float height, float textScale) {
 		if (!FLAlertLayer::init(p0, title, desc, btn1, btn2, width, scroll, height, textScale)) return false;
-		if (!shouldCopy || !levelToCopy) return true;
+		if (!title) return true;
+		if (!shouldCopy || !levelToCopy) {
+			if (!CCScene::get() || !CCScene::get()->getChildByType<LevelAreaInnerLayer>(0)) return true;
+
+			const int towerLevelID = levelNameToLevelID(title);
+			if (towerLevelID < 5001) return true;
+
+			auto level = typeinfo_cast<GJGameLevel*>(GameLevelManager::sharedState()->m_mainLevels->objectForKey(fmt::to_string(towerLevelID)));
+			if (!level || level->m_levelID.value() < 1) return true;
+			shouldCopy = true;
+			levelToCopy = level;
+		}
 
 		auto cButton = CCMenuItemSpriteExtra::create(
 			ButtonSprite::create("Copy Level", 80, 0, 0.6f, true, "goldFont.fnt", "GJ_button_04.png", 25.0f),
@@ -53,16 +85,4 @@ class $modify(MyLevelPage, LevelPage) {
 		shouldCopy = true;
 		levelToCopy = m_level;
 	}
-};
-
-class $modify(MyLevelAreaInnerLayer, LevelAreaInnerLayer) {
-	void onInfo(CCObject* sender) {
-		LevelAreaInnerLayer::onInfo(sender);
-		shouldCopy = false;
-		levelToCopy = nullptr;
-        auto level = typeinfo_cast<GJGameLevel*>(GameLevelManager::sharedState()->m_mainLevels->objectForKey(fmt::to_string(static_cast<CCMenuItemSpriteExtra*>(sender)->getTag())));
-		if (!level || level->m_levelID.value() < 1) return;
-		shouldCopy = true;
-		levelToCopy = level;
-    }
 };
